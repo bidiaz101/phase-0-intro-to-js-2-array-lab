ab6e81a2a850571ac35d1887cc39c2979f7365f1
Brian Diaz <bidiaz101@gmail.com>
2021-08-12 16:04:17 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-const cats = ["Milo","Otis","Garfield"];
+const cats = ["Milo", "Otis", "Garfield"];
+
 function destructivelyAppendCat(name) {
     return cats.push(name);
-}
+};
+
 function destructivelyPrependCat(name) {
     return cats.unshift(name);
-}
+};
+
 function destructivelyRemoveLastCat() {
     return cats.pop();
-}
+};
+
 function destructivelyRemoveFirstCat() {
     return cats.shift();
-}
+};
+
 function appendCat(name) {
-    return [...cats, name];
-}
+    const appendedCats = [...cats,name];
+    return appendedCats;
+};
+
 function prependCat(name) {
-    return [name, ...cats];
-}
+    const prependedCats = [name, ...cats];
+    return prependedCats;
+};
+
 function removeLastCat() {
-    return cats.slice(0,2);
-}
+    const noLastCat = cats.slice(0,cats.length-1);
+    return noLastCat;
+};
+
 function removeFirstCat() {
-    return cats.slice(1);
-}
+    const noFirstCat = cats.slice(1);
+    return noFirstCat;
+};
